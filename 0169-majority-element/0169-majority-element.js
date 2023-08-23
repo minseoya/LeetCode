873785624ef17dc8c5eb04e017a2a756bad9a992
minseoya <119482288+minseoya@users.noreply.github.com>
2023-08-23 11:23:18 +0900

    Time: 69 ms (37.92%), Space: 43.8 MB (61.76%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const counter = new Map()
+    for(let i=0;i<nums.length;i++){
+        if(!counter.has(nums[i])){
+            counter.set(nums[i],1)
+        }else{
+            counter.set(nums[i],counter.get(nums[i])+1)
+        }
+    }
+    console.log(counter)
+    let maxValue = 0;
+    let maxKey =0
+    counter.forEach((value, key, map) => {
+   if(value>=maxValue){
+       maxValue=value
+       maxKey=key
+   }
+});
+    return maxKey
+};
