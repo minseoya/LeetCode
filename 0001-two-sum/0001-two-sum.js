86b6b86bd88fb81021f85e9e7f16869737a4a6d6
minseoya <119482288+minseoya@users.noreply.github.com>
2023-08-30 17:36:27 +0900

    Time: 57 ms (83.54%), Space: 42.7 MB (42.85%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const map = new Map()
+    for(let i =0; i<nums.length;i++){
+        const result = target - nums[i]
+        if(map.has(result)){
+            return [map.get(result),i]
+        }
+        map.set(nums[i],i)
+    }
+    return 
+};
