510dd7be0d7287a74e673e905893d60853cc73c6
minseoya <119482288+minseoya@users.noreply.github.com>
2023-09-27 15:30:01 +0900

    Time: 48 ms (75.51%), Space: 42.6 MB (17.35%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if(nums.indexOf(target)>=0){
+        return (nums.indexOf(target))
+    }
+    if(nums[nums.length-1]<target){
+        return nums.length
+    }
+
+    for(let i =0;i<nums.length;i++){
+        if(nums[i]>target){
+            return i
+        }
+    }
+};
