16cec5ec942d08894c8f963ff9a521c4e84a8ca4
minseoya <119482288+minseoya@users.noreply.github.com>
2023-11-06 16:05:53 +0900

    Time: 49 ms (73.16%), Space: 42.3 MB (14.87%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -5,14 +5,8 @@
  */
 
 var removeElement = function(nums, val) {
-  let answers =0
 
-    for(let i = 0; i<nums.length;i++){
-      if(nums[i]!==val){
-        nums[answers]=(nums[i])
-        answers++
-      }
+    while(nums.includes(val)){
+        nums.splice(nums.indexOf(val), 1)
     }
-
-    return answers
 };
